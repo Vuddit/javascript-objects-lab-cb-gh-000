556c7209b20329d95ce7dc68425a3593e44b4a37
Rohit Kale <ruk22@cam.ac.uk>
2020-04-29 23:26:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  delete object["key"];
-  object.key = value;
+  var object = Object.assign({}, object, { [key]: value })
   return object
 }
