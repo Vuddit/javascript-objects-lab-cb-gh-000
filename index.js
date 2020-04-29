a2c7d84fc3bb163e8d6bc1d0c8daa65b2707f050
Rohit Kale <ruk22@cam.ac.uk>
2020-04-29 23:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var recipes = new Object({ ingredient: 'at least three' })
 
 function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, { key: 'value' })
+  console.log(object)
 }
+
+function updateObjectWithKeyAndValue
